f489a926df36ba5a82577615b40a6396f8bd646b
uki <ukiworks034@gmail.com>
2022-07-12 18:42:08 -0400

    camera page 80% done

--- react-app/src/components/CameraPage/CameraPage.js
+++ react-app/src/components/CameraPage/CameraPage.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, Redirect, useParams } from "react-router-dom";
-import EditCameraModal from "../EditCamera/EditCameraModal"
+import { Link, NavLink, Redirect, useParams } from "react-router-dom";
+import EditCameraModal from "../EditCamera/EditCameraModal";
+import ImageGallery from 'react-image-gallery';
 import './CameraPage.css'
+import '../../context/Buttons.css'
+import '../../context/Misc.css'
+
 
 function CameraPage() {
 
@@ -10,33 +14,85 @@ function CameraPage() {
 
     const sessionUser = useSelector((state) => state.session?.user);
     const cameras = useSelector((state) => state.cameras)
+    const users = useSelector((state) => state.users)
+    console.log("USERS: ", users)
 
     const { cameraId } = params;
 
     const currentCamera = cameras[cameraId]
     const productImagesArr = currentCamera?.images.filter(image => image.film_roll === false)
+    const filmRollArr = currentCamera?.images.filter(image => image.film_roll === true)
+    const cameraUser = users[currentCamera?.user_id]?.username
     // console.log("PROD IMAGES ARR: ", productImagesArr)
+
+    const [showFilmRoll, setShowFilmRoll] = useState(false);
+
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    let images = [];
+    let filmRoll = [];
+    productImagesArr?.forEach((image) => (
+        images.push({ "original": image.image_url, "thumbnail": image.image_url })
+    ))
+    filmRollArr?.forEach((image) => (
+        filmRoll.push({ "original": image.image_url, "thumbnail": image.image_url })
+    ))
+
+
     return (
-        <>
+        <div>
+
             {productImagesArr && (
-                <div>
-                    <div key={currentCamera.id}>
-                        <img src={productImagesArr[0]?.image_url} />
-                        <p>Brand: {currentCamera.brand}</p>
-                        <p>Model: {currentCamera.model}</p>
-                        <p>Film Type: {currentCamera.film_type}</p>
-                        <p>{currentCamera.other_specs}</p>
-                        <p>Amount: {currentCamera.amount}</p>
-                        <p>Inventory: {currentCamera.inventory}</p>
-                        <p>Posted on: {currentCamera.created_at}</p>
-                        <p>Sold by: {currentCamera?.user_id}</p>
+                <div id="camera-page">
+                    <div id="back-link-div">
+                        <i class="fa-solid fa-arrow-left"></i>
+                        <NavLink  to='/cameras' style={{ color: 'inherit', textDecoration: 'inherit' }}
+                        > <button id="back-link">Back to Catalogue</button>
+                        </NavLink>
                     </div>
-                    {currentCamera?.user_id === sessionUser?.id && (
-                        <EditCameraModal currentCamera={currentCamera}/>
-                    )}
+                    <div id="camera-section">
+                        <div id='carousel-div'>
+                            <div>
+                                <button className="carousel-bttn" onClick={() => setShowFilmRoll(false)}>Product Images</button>
+                                <button className="carousel-bttn" onClick={() => setShowFilmRoll(true)}>Film Roll</button>
+                            </div>
+                            {showFilmRoll ? <ImageGallery items={filmRoll} /> : <ImageGallery items={images} />}
+
+                        </div>
+                        <div id="camera-specs" key={currentCamera.id}>
+                            <p id="camera-title">{`${currentCamera.brand} ${currentCamera.model}`}</p>
+                            <p id="film-type">Film Type: {currentCamera.film_type}</p>
+                            <p id="other-specs">{currentCamera.other_specs}</p>
+                            <p id="price">${currentCamera.amount}</p>
+
+                            <div>
+                                {currentCamera.inventory < 2 ?
+                                    <p className="inventory" style={{ color: 'red' }}>Only {currentCamera.inventory} left in stock</p>
+                                    :
+                                    <p className="inventory">{currentCamera.inventory} left in stock</p>
+                                }
+                                <p>Sold by: {cameraUser}</p>
+                                <p>Posted on: {currentCamera.created_at}</p>
+                                {/* <p>{new Date(currentCamera?.updatedAt).toLocaleDateString(undefined, options)}</p> */}
+                                <button id="add-cart-btn">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        {currentCamera?.user_id === sessionUser?.id && (
+                            <EditCameraModal currentCamera={currentCamera} />
+                        )}
+                    </div>
+                    <div id="reviews-section">
+                        <p id="reviews-title">Reviews</p>
+                    </div>
                 </div>
             )}
-        </>
+        </div>
     )
 }
 
