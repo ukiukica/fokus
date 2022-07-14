2fc6f73d6b760a1e6f89e0032580ec45cb79854b
uki <ukiworks034@gmail.com>
2022-07-14 18:31:12 -0400

    basic functionality of shopping cart added

---
+++ react-app/src/components/ShoppingCart/ShoppingCompleted.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, NavLink, Redirect } from "react-router-dom";
+
+function ShoppingCompleted () {
+
+    return (
+        <>
+            <p>Thank you for your purchase!</p>
+        </>
+    )
+}
+
+export default ShoppingCompleted;
